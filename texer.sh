7c473ca2b59cef6a94c1bd4d4351107d91448560
sriniv27 <sriniv27@purdue.edu>
2016-04-06 12:32:45 -0400

    Added changes to look for and suggest files

--- texer.sh
+++ texer.sh
@@ -4,23 +4,32 @@
 
 
 
-
 # Actual code
 
 ## Set up working directory
 echo "\t Welcome to AutoTeXeR. I will be your guide.\n\n\n"
 read -p "Are you in the directory you want to be? If yes press ENTER else type in the path of the directory you want to work in: " directory;
-directory=${directory-.}
-if [ -d "$directory" ];
+#directory=${directory-.}
+echo "$directory"
+if [ -r "$directory" ];
 then
     echo "Found $directory";
 else
     read -p "Directory not found. Do you want to create it? [yn] " yn;
     case $yn in
         [yY]*)mkdir -p $directory;;
-        [nN]*) echo "Nothing created. Defaulting to current directory";;
+        [nN]*) echo "Nothing created. Defaulting to current directory";directory=".";;
     esac
 fi
+
+## Set up Working files
+shopt -s nullglob
+filearray=("$directory/"*.tex)
+PS3="The following TeX files were found in $directory. Do you want to use any of these?"
+select texfile in "${filearray[@]}";
+do
+    echo "$texfile";break;
+done
 
 ## Select compiler to use
 compilers=("latex" "xelatex" "pdflatex" "latexmk")
